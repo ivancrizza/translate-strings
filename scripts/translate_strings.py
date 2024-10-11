a56e294bf6cab2aa071a61b1becb34ae152c0ef8
Crizza <48035639+ivancrizza@users.noreply.github.com>
2024-10-11 15:45:31 -0300

    Update translate_strings.py

--- scripts/translate_strings.py
+++ scripts/translate_strings.py
@@ -1,5 +1,8 @@
 import xml.etree.ElementTree as ET
 from googletrans import Translator
+import os
+
+strings_file = os.path.join(os.getcwd(), 'app/src/main/res/values/strings.xml')
 
 
 def translate_text(text, target_language='fr'):
@@ -8,23 +11,19 @@ def translate_text(text, target_language='fr'):
     return translation.text
 
 
-strings_file = 'app/src/main/res/values/strings.xml'
 output_file = 'app/src/main/res/values-fr/strings_fr.xml'
-
 
 tree = ET.parse(strings_file)
 root = tree.getroot()
-
 
 for string in root.findall('string'):
     original_text = string.text
     translated_text = translate_text(original_text, 'fr')
     string.text = translated_text
 
-
 import os
-os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
+os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
 tree.write(output_file, encoding="utf-8", xml_declaration=True)
 
