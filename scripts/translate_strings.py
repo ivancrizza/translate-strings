23cff3b1eb1cd77988213727580d4f3e90a025f6
Crizza <48035639+ivancrizza@users.noreply.github.com>
2024-10-11 16:07:30 -0300

    Update translate_strings.py

--- scripts/translate_strings.py
+++ scripts/translate_strings.py
@@ -1,8 +1,6 @@
+import os
 import xml.etree.ElementTree as ET
 from googletrans import Translator
-import os
-
-strings_file = os.path.join(os.getcwd(), './android/app/src/main/res/values/strings.xml')
 
 
 def translate_text(text, target_language='fr'):
@@ -11,19 +9,23 @@ def translate_text(text, target_language='fr'):
     return translation.text
 
 
-output_file = './android/app/src/main/res/values-fr/strings_fr.xml'
+strings_file = './android/app/src/main/res/values/strings.xml'
+output_dir = './android/app/src/main/res/values-fr'
+output_file = os.path.join(output_dir, 'strings_fr.xml')
+
+
+os.makedirs(output_dir, exist_ok=True)
+
 
 tree = ET.parse(strings_file)
 root = tree.getroot()
+
 
 for string in root.findall('string'):
     original_text = string.text
     translated_text = translate_text(original_text, 'fr')
     string.text = translated_text
 
-import os
-
-os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
 tree.write(output_file, encoding="utf-8", xml_declaration=True)
 
